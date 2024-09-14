52e9206452d6df9c34616ad61d3068d8cb6ba2d7
Maisy Capps <maisycapps@gmail.com>
2024-09-14 16:32:28 -0700

    schema design: Posts relationship to Destinations

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const apiRoutes = require("./api/index");
+const apiRoutes = require("./API/index");
 require("dotenv").config();
 
 //middleware
