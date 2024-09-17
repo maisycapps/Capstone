69189f9d831f41bbc55740211c2224384e6ca6bc
Maisy Capps <maisycapps@gmail.com>
2024-09-17 11:14:26 -0700

    debugging migration

--- prisma/seed.js
+++ prisma/seed.js
@@ -10,25 +10,53 @@ const user = Array.from({ length: 30 }).map(() => ({
   email: faker.internet.email(),
   password: faker.internet.password(),
   bio: faker.person.bio(),
+  profileImg: faker.image.urlLoremFlickr({ width: 300,height: 300, category: 'portraits' })
 }));
 
 //seeds destinations
 const destinationData = Array.from({ length: 30 }).map(() => ({
-  destinationName: faker.location.city({ skipDuplication: true }),
+  destinationName: faker.location.city({ skipDuplication: false }),
+  destinationImg: faker.image.urlLoremFlickr({ width: 300,height: 300, category: 'cities' })
 }));
 
-//trips seed
-// const tripsData = Array.from({ length: 5 }).map(() => ({
-//   userId: 10,
-//   destinationId: 25,
-//   tripName: faker.company.name(),
-//   end: faker.date.soon(),
-// }));
+//seeds trips
+const tripsData = Array.from({ length: 5 }).map(() => ({
+  userId: faker.number.int({ min: 1, max: 30 }),
+  destinationId: faker.number.int({ min: 1, max: 30 }),
+  tripName: faker.word.words(),
+  startDate: faker.date.soon(),
+  endDate: faker.date.soon(),
+}));
+
+//seeds posts
+const postData = Array.from({ length: 30 }).map(() => ({
+  userId: faker.number.int({ min: 1, max: 30 }),
+  text: faker.lorem.sentences(3),
+  postImg: faker.image.urlLoremFlickr({ width: 300,height: 300, category: 'cities' }),
+  destinationId: faker.number.int({ min: 1, max: 30 }),
+}));
+
+//seeds comments
+const commentData = Array.from({ length: 30 }).map(() => ({
+  userId: faker.number.int({ min: 1, max: 30 }),
+  text: faker.lorem.sentences(1),
+  postId: faker.number.int({ min: 1, max: 30 }),
+}));
+
+//seeds likes
+const likeData = Array.from({ length: 30 }).map(() => ({
+  userId: faker.number.int({ min: 1, max: 30 }),
+  postId: faker.number.int({ min: 1, max: 30 }),
+}));
 
 const seed = async () => {
   await prisma.users.createMany({ data: user });
   await prisma.destinations.createMany({ data: destinationData });
-  // await prisma.trips.createMany({ data: tripsData });
+  await prisma.trips.createMany({ data: tripsData });
+  await prisma.posts.createMany({ data: postData });
+  await prisma.comments.createMany({ data: commentData });
+  await prisma.likes.createMany({ data: likeData });
+
 };
 seed()
   .then(async () => await prisma.$disconnect())
