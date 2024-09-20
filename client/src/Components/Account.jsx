20c1bfe4e35cbe58b1a96ac193b689a285f2f377
Maisy Capps <maisycapps@gmail.com>
2024-09-20 11:25:05 -0700

    lunchtime commit

--- client/src/Components/Account.jsx
+++ client/src/Components/Account.jsx
@@ -9,31 +9,48 @@ const Account = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetching user authentication data
-        const response = await axios.get(`http://localhost:3000/api/auth/account/users`);
-        const accountData = response.data;
+
+        const token = localStorage.getItem("token");
+
+        // Fetch user's data
+        const response = await axios.get("http://localhost:3000/api/auth/account/users", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+      });
+        const accountData = await response.data[0];
         setUser(accountData);
 
       } catch (error) {
         console.error("Error fetching user account data", error);
+
       }
-    };
+  };
 
     fetchData();
+    console.log("user", user)
   }, []); 
 
   return (
+    <>
     <div className={styles.accountCard}>
-      {user? ( 
+      
+      {user ? ( 
         <div className={styles.account}>
-          <img src={italy} alt="User Account" />
+
+          {user.profileImg ? (
+          <img src={user.profileImg} alt="Profile Image" /> ) : (<img src={italy} alt="Default Profile Image" />  )}
+
           <h3>Welcome to your account, {user.firstName} {user.lastName}</h3>
-          
+          <h5>{user.userName}</h5>
+          {user.bio ? <p>{user.bio}</p> : null}
         </div>
-      ) : (
-        <p>Loading account details...</p>
-      )}
+        ) : (
+          <p>Loading account details...</p>
+      )};
+   
     </div>
+    </>
   );
 };
 
