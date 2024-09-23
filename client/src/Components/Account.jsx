efab607964d32229ecf15b24524b751752f7e485
Maisy Capps <maisycapps@gmail.com>
2024-09-23 07:25:21 -0700

    commit pre-merge

--- client/src/Components/Account.jsx
+++ client/src/Components/Account.jsx
@@ -19,6 +19,7 @@ const Account = () => {
           },
       });
         const accountData = await response.data[0];
+        console.log(accountData)
         setUser(accountData);
 
       } catch (error) {
@@ -28,8 +29,8 @@ const Account = () => {
   };
 
     fetchData();
-    console.log("user", user)
   }, []); 
+
 
   return (
     <>
@@ -41,9 +42,20 @@ const Account = () => {
           {user.profileImg ? (
           <img src={user.profileImg} alt="Profile Image" /> ) : (<img src={italy} alt="Default Profile Image" />  )}
 
-          <h3>Welcome to your account, {user.firstName} {user.lastName}</h3>
-          <h5>{user.userName}</h5>
-          {user.bio ? <p>{user.bio}</p> : null}
+          <h3>Welcome to your account, {user.firstName} {user.lastName}!</h3>
+          <h5>Username: {user.userName}</h5>
+          {user.bio ? <p>Bio: {user.bio}</p> : null}
+
+          {/* {user.likes}
+          {user.posts}
+          {user.trips}   */}
+          
+          {/* who the user follows*/}
+          {/* {user.followedBy}  */}
+
+          {/* who the user is followed by*/}
+          {/* {user.following} */}
+
         </div>
         ) : (
           <p>Loading account details...</p>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+
+// if you are seeing duplicate console.logs in your terminal -- <StrictMode> calls useEffects twice when in development mode.
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <StrictMode>  
     <BrowserRouter>
       <App />
     </BrowserRouter>
