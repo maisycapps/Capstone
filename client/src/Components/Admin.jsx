41eef4c45bab819b1c3f5ae3b9a4ea190fb6844d
jordanharris0 <jordanharris1833@yahoo.com>
2024-09-30 16:07:58 -0500

    finished admin panel

---
+++ client/src/Components/Admin.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Users from "./AdminComponents/UsersTab";
+import Posts from "./AdminComponents/PostsTab";
+import Trips from "./AdminComponents/TripsTab";
+import Destinations from "./AdminComponents/DestinationsTab";
+
+const Admin = () => {
+  const [activeTab, setActiveTab] = useState("users");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "users":
+        return <Users />;
+      case "posts":
+        return <Posts />;
+      case "trips":
+        return <Trips />; // Placeholder for now
+      case "destinations":
+        return <Destinations />; // Placeholder for now
+      default:
+        return <div>Select a tab</div>;
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {" "}
+        <div>
+          <h2>Admin Dashboard</h2>
+          <div className="admin-tabs">
+            <button onClick={() => setActiveTab("users")}>Users</button>
+            <button onClick={() => setActiveTab("posts")}>Posts</button>
+            <button onClick={() => setActiveTab("trips")}>Trips</button>
+            <button onClick={() => setActiveTab("destinations")}>
+              Destinations
+            </button>
+          </div>
+
+          <div className="admin-content">{renderTabContent()}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
