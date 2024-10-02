040c554dd057be4ad0a6eb8ce209c57d4bb03c41
Maisy Capps <maisycapps@gmail.com>
2024-10-02 09:32:34 -0700

    revised about us text

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -20,20 +20,16 @@ function About() {
         <img src={italy} alt="about" className={styles.aboutImage} />
         <div className={styles.aboutText}>
           <h1>
-            Copilot 🎈 is a Social interaction platform where individuals can
-            connect with people, and make friends, plan trips, and share
-            memories together.
-          </h1>
+            Copilot is a user-friendly social media app designed for travelers. Register for an account to create and manage trips, and create posts to document and share your experiences. Curious about what other travelers are doing? Follow other users, view their posts, and leave a comment or like. 
+            </h1>
           <p>
-            Developed by Jordan Harris, Maisy Capps, and Mathew Correa, Copilot
-            aims to bring people together through shared adventures, offering
-            features that allow users to organize trips, follow friends, and
-            engage with travel posts, making it a hub for both social and travel
-            enthusiasts.
-          </p>
+             We aim to bring people together through shared adventures. By offering features that allow users to organize trips, follow friends, and engage with travel posts, Copilot is a hub for both social and travel enthusiasts.
+            
+          </p><br />
+          <p>Developed by Jordan Harris, Maisy Capps, and Mathew Correa.</p>
         </div>
       </div>
-      <h1 className={styles.slog}>Social Interaction Made Easy </h1>
+      <h1 className={styles.slog}>Go places with Copilot</h1>
 
       <div className={styles.memorize}>
         <div className={styles.imgDisplay}>
